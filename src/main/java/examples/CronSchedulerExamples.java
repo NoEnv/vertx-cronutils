2505b1e065495973eeb03f472afd4e05ef432c97
Lukas Prettenthaler <lukas@noenv.com>
2020-08-04 14:12:36 +0200

    add example3

--- src/main/java/examples/CronSchedulerExamples.java
+++ src/main/java/examples/CronSchedulerExamples.java
@@ -1,8 +1,15 @@
 package examples;
 
+import com.cronutils.builder.CronBuilder;
+import com.cronutils.model.Cron;
 import com.cronutils.model.CronType;
+import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.noenv.cronutils.CronScheduler;
 import io.vertx.core.Vertx;
+
+import static com.cronutils.model.CronType.UNIX;
+import static com.cronutils.model.field.expression.FieldExpressionFactory.always;
+import static com.cronutils.model.field.expression.FieldExpressionFactory.between;
 
 /**
  * @author Lukas Prettenthaler
@@ -23,6 +30,22 @@ public class CronSchedulerExamples {
       .schedule(s -> {
         s.cancel();
         System.out.println("timer triggered and canceled");
+      });
+  }
+
+  public void example3(Vertx vertx) {
+    Cron cronExpression = CronBuilder
+      .cron(CronDefinitionBuilder.instanceDefinitionFor(UNIX))
+      .withDoM(between(1, 3))
+      .withMonth(always())
+      .withDoW(always())
+      .withHour(always())
+      .withMinute(always()).instance();
+
+    CronScheduler
+      .create(vertx, cronExpression.asString())
+      .schedule(s -> {
+        System.out.println("timer triggered");
       });
   }
 }
